68848046b40fb53d16a4659a12ef681091efccdd
BharatKumarRawal <rawalb494@gmail.com>
2025-01-08 11:10:15 +0545

    pages and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! <span className="text-red-500">Red</span>
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+const Signup = () => {
+  return (
+    <div>
+      <h1>SignOut</h1>
+    </div>
+  );
+};
+
+export default Signup;
