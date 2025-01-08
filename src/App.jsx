02443ded6bc40c5870db261a89a7b6d1d83382ce
BharatKumarRawal <rawalb494@gmail.com>
2025-01-08 09:58:07 +0545

    install reactjs and configured tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world! <span className="text-red-500">Red</span>
+    </h1>
+  );
+};
+
+export default App;
