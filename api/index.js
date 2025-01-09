e6b3d607e72153515753d7bc1bfa5c8d948747d9
BharatKumarRawal <rawalb494@gmail.com>
2025-01-09 08:54:36 +0545

    connection established with database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!!");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
