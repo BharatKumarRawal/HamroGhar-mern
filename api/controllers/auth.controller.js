9d40e3d3f50a6fbb309cce1964cdddf9e2ef1065
BharatKumarRawal <rawalb494@gmail.com>
2025-01-09 20:24:10 +0545

    sigin api, controller and page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
+const errorHandler = require("../utils/error.js");
+const jwt = require("jsonwebtoken");
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -12,4 +14,29 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log(email);
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const isValidPassword = bcrypt.compareSync(password, validUser.password);
+    if (!isValidPassword) {
+      return next(errorHandler(401, "Invalid credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 12 * 60 * 60 * 1000,
+    });
+    const { password: pass, ...user } = validUser._doc;
+    res.status(200).json({ message: "User logged in successfully", user });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { signup, signin };
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
 const Signin = () => {
   return (
-    <div>
-      <h1>Signin</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          className="bg-slate-100 p-3 rounded-lg border"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-slate-100 p-3 rounded-lg border"
+        />
+        <button className="bg-green-600 text-white py-3 rounded-lg hover:opacity-90 uppercase">
+          Signin
+        </button>
+      </form>
+      <div className="flex gap-4 mt-4">
+        <p>Don't have an account?</p>
+        <Link to="/sign-up" className="text-blue-500 hover:text-red-600">
+          Sign Up
+        </Link>
+      </div>
     </div>
   );
 };
